d89b7ace65b4e089ffe7f8501c7f7cca20b6271a
lswatson16 <lauralynw16@gmail.com>
2022-03-24 17:47:27 -0400

    Add images to MovieDetail

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -6,6 +6,8 @@ function MovieDetail({ movie, returnToMain }) {
       <h1>{movie.title}</h1>
       <h2>{movie.release_date}</h2>
       <h2>{movie.average_rating}</h2>
+      <img src={movie.poster_path}/>
+      <img src={movie.backdrop_path}/>
       <button onClick={()=>returnToMain()}>Go Back</button>
 
     </div>
