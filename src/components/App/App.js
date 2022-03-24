3a3371ad150c29098110afe846958962905fa597
Emili Kaiman <emilikaiman@gmail.com>
2022-03-24 16:39:34 -0600

    Use API data to set state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,32 +4,48 @@ import React, { Component } from 'react';
 import movieData from '../../movieData';
 import MovieWrapper from '../MovieWrapper/MovieWrapper.js';
 import MovieDetail from '../MovieDetail/MovieDetail';
+import apiCalls from '../../ApiCalls';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies,
+      movies: [],
       selectedMovie: [],
       movieClicked: false
     };
   }
 
-  displayOneMovie = (id) => {
-    console.log('clicked', id)
-    const foundMovie = this.state.movies.find(movie => movie.id === id)
-    this.setState({ selectedMovie: foundMovie, movieClicked: true })
-  }
+  displayOneMovie = id => {
+    console.log('clicked', id);
+    const foundMovie = this.state.movies.find(movie => movie.id === id);
+    this.setState({ selectedMovie: foundMovie, movieClicked: true });
+  };
 
   returnToMain = () => {
-    this.setState({ movieClicked: false })
+    this.setState({ movieClicked: false });
+  };
+
+  componentDidMount() {
+    
+    apiCalls.fetchData().then(data => this.setState({ movies: data.movies }));
   }
 
   render() {
     return (
       <div>
-        {this.state.movieClicked && <MovieDetail movie={this.state.selectedMovie} returnToMain={this.returnToMain}/>}
-        {!this.state.movieClicked && <MovieWrapper movies={this.state.movies} displayOneMovie={this.displayOneMovie}/>}
+        {this.state.movieClicked && (
+          <MovieDetail
+            movie={this.state.selectedMovie}
+            returnToMain={this.returnToMain}
+          />
+        )}
+        {!this.state.movieClicked && (
+          <MovieWrapper
+            movies={this.state.movies}
+            displayOneMovie={this.displayOneMovie}
+          />
+        )}
       </div>
     );
   }
