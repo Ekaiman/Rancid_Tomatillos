de3bb36cf139f81b95426d829a174723a8834717
lswatson16 <lauralynw16@gmail.com>
2022-03-24 17:48:40 -0400

    Rearrange button

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -3,12 +3,12 @@ import React from 'react';
 function MovieDetail({ movie, returnToMain }) {
   return (
     <div className='movieDetail'>
+      <button onClick={()=>returnToMain()}>Go Back</button>
       <h1>{movie.title}</h1>
       <h2>{movie.release_date}</h2>
       <h2>{movie.average_rating}</h2>
       <img src={movie.poster_path}/>
       <img src={movie.backdrop_path}/>
-      <button onClick={()=>returnToMain()}>Go Back</button>
 
     </div>
   )
