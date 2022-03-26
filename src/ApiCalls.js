2a35199b89d615afd6d41c6cd5c46f3f4c71ccde
Emili Kaiman <emilikaiman@gmail.com>
2022-03-26 12:28:15 -0600

    Create fetch for one movie

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,8 +1,14 @@
 const apiCalls = {
-
   fetchData() {
-    return fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies").then(response => response.json())
+    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(
+      response => response.json()
+    );
   },
 
-}
+  fetchOneMovie(id) {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then(
+      response => response.json()
+    );
+  }
+};
 export default apiCalls
