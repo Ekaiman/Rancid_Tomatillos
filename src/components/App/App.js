dda2c3d093f45a500d50f4523af8d53c69f8e0dd
lswatson16 <lauralynw16@gmail.com>
2022-03-28 18:25:22 -0400

    Remove comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,21 +20,7 @@ class App extends Component {
   displayOneMovie = id => {
     console.log('clicked id', id);
     this.setState({ movieClicked: true, selectedMovieId: id })
-    // const foundMovie = this.state.movies.find(movie => movie.id === id);
-    // this.setState({ selectedMovie: foundMovie, movieClicked: true });
   };
-
-  // displayOneMovie = id => {
-  //   apiCalls
-  //     .fetchOneMovie(id)
-  //     .then(data =>
-  //       this.setState({ selectedMovie: data.movie, movieClicked: true })
-  //     )
-  //     .catch(error => {
-  //       console.log('caught err for single movie')
-  //       this.setState({ error: 'Sorry our team is working on resolving this issue' })
-  //     })
-  // };
 
   returnToMain = () => {
     this.setState({ movieClicked: false });
