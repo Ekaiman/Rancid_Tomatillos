12b9bb105a8435fd256d709698d40a659f278f99
lswatson16 <lauralynw16@gmail.com>
2022-03-28 18:22:17 -0400

    Add error handling

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,14 +1,28 @@
 const apiCalls = {
   fetchData() {
-    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(
-      response => response.json()
-    );
+    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => {
+        console.log('response', response)
+        console.log('status', response.status)
+        return response.json()
+      })
+      .then(data => data)
+      .catch(error => {
+        console.log('err typeof', typeof error)
+        console.log('err name', error.name)
+        console.log('err msg', error.message)
+        throw error
+      })
   },
 
   fetchOneMovie(id) {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then(
-      response => response.json()
-    );
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+      .then(response => response.json())
+      .then(data => data)
+      .catch(error => {
+        throw error
+      })
   }
 };
+
 export default apiCalls;
