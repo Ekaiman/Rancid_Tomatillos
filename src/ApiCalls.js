e8e1d2f53a9a1c81ddf51dc320c663cfec9cd963
lswatson16 <lauralynw16@gmail.com>
2022-03-28 08:21:30 -0400

    Fix merge conflict issue in apiCalls

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -5,5 +5,10 @@ const apiCalls = {
     );
   },
 
-}
+  fetchOneMovie(id) {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then(
+      response => response.json()
+    );
+  }
+};
 export default apiCalls;
