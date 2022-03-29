17fbc9693f3b29fa92ecd1c79515ff6a11f02f5f
lswatson16 <lauralynw16@gmail.com>
2022-03-29 15:11:35 -0400

    Refactor apiCalls

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -6,22 +6,11 @@ const apiCalls = {
         console.log('status', response.status)
         return response.json()
       })
-      .then(data => data)
-      .catch(error => {
-        console.log('err typeof', typeof error)
-        console.log('err name', error.name)
-        console.log('err msg', error.message)
-        throw error
-      })
   },
 
   fetchOneMovie(id) {
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
       .then(response => response.json())
-      .then(data => data)
-      .catch(error => {
-        throw error
-      })
   }
 };
 
