d7a640d29e76e9cf8ea05643602e04ad14aa4f4a
Emili Kaiman <emilikaiman@gmail.com>
2022-03-31 15:41:32 -0600

    Set state to false

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -6,68 +6,75 @@ import ErrorHandling from '../ErrorHandling/ErrorHandling';
 
 class MovieDetail extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      selectedMovie: {},
+      selectedMovie: false,
       error: false
-    }
+    };
   }
 
   componentDidMount() {
-    console.log("calling single movie api", this.props)
-    const { movieId } = this.props
-    console.log('movieId', movieId)
+    console.log('calling single movie api', this.props);
+    const { movieId } = this.props;
+    console.log('movieId', movieId);
     apiCalls
       .fetchOneMovie(movieId)
-      .then(data =>
-        this.setState({ selectedMovie: data.movie })
-      )
+      .then(data => this.setState({ selectedMovie: data.movie }))
       .catch(error => {
-        console.log('caught err for single movie')
-        this.setState({ error: 'Sorry our team is working on resolving this issue' })
-      })
+        console.log('caught err for single movie');
+        this.setState({
+          error: 'Sorry our team is working on resolving this issue'
+        });
+      });
+  }
+
+  roundAverage() {
+    if (this.state.selectedMovie){
+
+      console.log(this.state.selectedMovie.average_rating);
+     return this.state.selectedMovie.average_rating.toFixed(2)
+    }
   }
 
   render() {
-    console.log('rendering movie')
-    const { selectedMovie: movie } = this.state
-      return (
+    console.log('rendering movie');
+    const { selectedMovie: movie } = this.state;
+    return (
+      <section className='movie-detail-background'>
         <div
           className='movieDetail'
           style={{
             backgroundImage: `url(${movie.backdrop_path})`
           }}
         >
-          {!this.state.error && <div className='gradient'>
-            
-            <Link to='/'>
-              <button className='view-all-movies-button'>
-              View All Movies
-              </button>
-            </Link>
-            {/* <img className='back-drop' src={movie.backdrop_path} /> */}
-            <section className='title-poster'>
-              <img className='poster' src={movie.poster_path} />
-              <div className='text-wrapper'>
-                <h1>{movie.title}</h1>
-                <h2 className='release-date'>Released: {movie.release_date}</h2>
-                <h2 className='rating'>Average rating: {movie.average_rating}</h2>
-                <h2 className='overview'> {movie.overview}</h2>
-              </div>
-            </section>
-          </div>
-          }
-
-          {this.state.error && (
-            <ErrorHandling
-              error={this.state.error}
-            />
+          {!this.state.error && (
+            <div className='gradient'>
+              <Link to='/'>
+                <button className='view-all-movies-button'>
+                  View All Movies
+                </button>
+              </Link>
+              <section className='title-poster'>
+                <img className='poster' src={movie.poster_path} />
+                <div className='text-wrapper'>
+                  <h1>{movie.title}</h1>
+                  <h2 className='release-date'>
+                    Released: {movie.release_date}
+                  </h2>
+                  <h2 className='rating'>
+                    Average rating: {this.roundAverage()}
+                  </h2>
+                  <h2 className='overview'> {movie.overview}</h2>
+                </div>
+              </section>
+            </div>
           )}
-        </div>
-      );
 
+          {this.state.error && <ErrorHandling error={this.state.error} />}
+        </div>
+      </section>
+    );
   }
 }
-
 
 export default MovieDetail;
