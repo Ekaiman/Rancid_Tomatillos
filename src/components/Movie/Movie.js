ca057b42bbac25a1dc130724b9d847e655b56000
Emili Kaiman <emilikaiman@gmail.com>
2022-03-31 15:40:29 -0600

    Formatting

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Movie.css';
 
-function Movie({ rating, backDropPath, id, posterPath, releaseDate, title, displayOneMovie }) {
+function Movie({
+  rating,
+  backDropPath,
+  id,
+  posterPath,
+  releaseDate,
+  title,
+  displayOneMovie
+}) {
   // console.log('id in movie', id)
   return (
     <div className='movie'>
-      <img onClick={()=> (displayOneMovie(id))} className='poster-image' src={posterPath} alt=''/>
+      <img
+        onClick={() => displayOneMovie(id)}
+        className='poster-image'
+        src={posterPath}
+        alt=''
+      />
       <h1>{title}</h1>
     </div>
   );
