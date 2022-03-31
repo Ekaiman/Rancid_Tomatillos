dd9878822c1f7817a7a3794deb198a186d6dd2c8
Emili Kaiman <emilikaiman@gmail.com>
2022-03-30 18:23:34 -0600

    Begin cypress testing

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -6,43 +6,43 @@ import ErrorHandling from '../ErrorHandling/ErrorHandling';
 
 class MovieDetail extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       selectedMovie: {},
       error: false
-    }
+    };
   }
 
   componentDidMount() {
-    console.log("calling single movie api", this.props)
-    const { movieId } = this.props
-    console.log('movieId', movieId)
+    console.log('calling single movie api', this.props);
+    const { movieId } = this.props;
+    console.log('movieId', movieId);
     apiCalls
       .fetchOneMovie(movieId)
-      .then(data =>
-        this.setState({ selectedMovie: data.movie })
-      )
+      .then(data => this.setState({ selectedMovie: data.movie }))
       .catch(error => {
-        console.log('caught err for single movie')
-        this.setState({ error: 'Sorry our team is working on resolving this issue' })
-      })
+        console.log('caught err for single movie');
+        this.setState({
+          error: 'Sorry our team is working on resolving this issue'
+        });
+      });
   }
 
   render() {
-    console.log('rendering movie')
-    const { selectedMovie: movie } = this.state
-      return (
-        <div
-          className='movieDetail'
-          style={{
-            backgroundImage: `url(${movie.backdrop_path})`
-          }}
-        >
-          {!this.state.error && <div className='gradient'>
-            
+    console.log('rendering movie');
+    const { selectedMovie: movie } = this.state;
+    return (
+      <div
+        className='movieDetail'
+        style={{
+          backgroundImage: `url(${movie.backdrop_path})`
+        }}
+      >
+        {!this.state.error && (
+          <div className='gradient'>
             <Link to='/'>
               <button className='view-all-movies-button'>
-              View All Movies
+                View All Movies
               </button>
             </Link>
             {/* <img className='back-drop' src={movie.backdrop_path} /> */}
@@ -51,23 +51,19 @@ class MovieDetail extends Component {
               <div className='text-wrapper'>
                 <h1>{movie.title}</h1>
                 <h2 className='release-date'>Released: {movie.release_date}</h2>
-                <h2 className='rating'>Average rating: {movie.average_rating}</h2>
+                <h2 className='rating'>
+                  Average rating: {movie.average_rating}
+                </h2>
                 <h2 className='overview'> {movie.overview}</h2>
               </div>
             </section>
           </div>
-          }
+        )}
 
-          {this.state.error && (
-            <ErrorHandling
-              error={this.state.error}
-            />
-          )}
-        </div>
-      );
-
+        {this.state.error && <ErrorHandling error={this.state.error} />}
+      </div>
+    );
   }
 }
-
 
 export default MovieDetail;
