e1b6178da2dfda3dec8248ab7a47d2d7274e4595
Emili Kaiman <emilikaiman@gmail.com>
2022-03-31 15:39:50 -0600

    Add title and generate random movie

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,62 +14,71 @@ class App extends Component {
       selectedMovie: null,
       selectedMovieId: null,
       movieClicked: false,
-      error: false
+      error: false,
+      randomImg: false
     };
   }
 
   displayOneMovie = id => {
     console.log('clicked id', id);
-    this.setState({ movieClicked: true, selectedMovieId: id })
+    this.setState({ movieClicked: true, selectedMovieId: id });
   };
 
   componentDidMount() {
     apiCalls
       .fetchData()
-      .then(data => this.setState({ movies: data.movies }))
-      .catch(error => {
-        console.log('caught err for ALL MOVIES', error)
-        this.setState({ error: 'Sorry our team is working on resolving this issue' })
+      .then(data => {
+        this.setState({
+          movies: data.movies,
+          randomImg: data.movies[Math.floor(Math.random() * data.movies.length)]
+        });
+        // this.state.randomImage =
+        // data.movies[Math.floor(Math.random() * data.movies.length)];
+        console.log(data.movies[Math.floor(Math.random() * data.movies.length)])
+        console.log(this.state.randomImg);
       })
+      .catch(error => {
+        console.log('caught err for ALL MOVIES', error);
+        this.setState({
+          error: 'Sorry our team is working on resolving this issue'
+        });
+      });
+    
   }
 
   render() {
     return (
       <div className='main-background'>
-  
-        <Route exact path='/'
-          render={() =>
-            <MovieWrapper
-              movies={this.state.movies}
-              displayOneMovie={this.displayOneMovie}
-            />}
-        />
-
-        <Route exact path='/movies/:movieId'
-          render={({ match }) => {
-            console.log('match', match)
-            return (
-              <MovieDetail
-                movieId={match.params.movieId}
+        <header className='title'>Rancid Tomatillos</header>
+        <section className='movie-holder'>
+          <Route
+            exact
+            path='/'
+            style={{
+              textDecoration: 'none'
+            }}
+            render={() => (
+              <MovieWrapper
+                movies={this.state.movies}
+                displayOneMovie={this.displayOneMovie}
+                randomImg={this.state.randomImg}
               />
-            )
+            )}
+          />
+        </section>
+
+        <Route
+          exact
+          path='/:movieId'
+          render={({ match }) => {
+            console.log('match', match);
+            return <MovieDetail movieId={match.params.movieId} />;
           }}
         />
 
-        {this.state.error && (
-          <ErrorHandling
-            error={this.state.error}
-          />
-        )}
-
-           
-
-
-      
-        
-
+        {this.state.error && <ErrorHandling error={this.state.error} />}
       </div>
-    )
+    );
   }
 }
 
