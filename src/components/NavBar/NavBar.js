f3718069d5cbad4b8bce96bf53937208990746f1
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-04-01 14:06:38 -0600

    Add Navbar for sorting

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Movie from '../Movie/Movie.js';
+import { NavLink, Link } from 'react-router-dom';
+import './NavBar.css';
+import '../../images/sort-abc.svg';
+import '../../images/sort-rating.svg';
+
+function NavBar({ sortMovies }) {
+  return (
+    <nav className='sort-bar'>
+      <div className='holder' onClick={() => sortMovies('alphabetically')}>
+        <p className='sort'></p>
+        <p>Sort A-Z</p>
+      </div>
+      <div className='holder' onClick={() => sortMovies('rating')}>
+        <p className='sort'></p>
+        <p>Sort by Highest Rating</p>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
