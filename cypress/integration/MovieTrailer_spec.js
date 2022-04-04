48ebb9f4ed556524626ca87ecc9162a1de4a325a
lswatson16 <lauralynw16@gmail.com>
2022-04-04 09:36:41 -0400

    Update MovieTrailer_spec.js

--- cypress/integration/MovieTrailer_spec.js
+++ cypress/integration/MovieTrailer_spec.js
@@ -14,24 +14,26 @@ describe('Rancid Tomatillos movie trailer page', () => {
       .and('match', /Rubik Glitch/)
   });
 
-  it('should display a movie trailer video', () => {
-    cy.get('iframe')
-      .should('have.attr', 'title')
-      .and('match', /YouTube video player/)
-  });
-
   it('should be able to navigate to the movies page', () => {
-    cy.get('.view-btn')
+    cy.get('button')
+      .should('have.class', 'view-btn')
       .contains('View All Movies')
       .click()
       .get('.random-movie-title')
   });
 
   it('should be able to navigate to the movie details page', () => {
-    cy.get('.view-btn')
+    cy.get('button')
+      .should('have.class', 'view-btn')
       .contains('View Movie Details')
       .click()
       .get('h1')
       .contains('The Owners')
+  });
+
+  it('should display a movie trailer video', () => {
+    cy.get('iframe')
+      .should('have.attr', 'title')
+      .and('match', /YouTube video player/)
   });
 });
