e179a65a3a78426719f32bc62b7163f8ad1ebac7
lswatson16 <lauralynw16@gmail.com>
2022-04-03 21:14:43 -0400

    Add cypress testing for movie trailer view

---
+++ cypress/integration/MovieTrailer_spec.js
@@ -0,0 +1,28 @@
+beforeEach(() => {
+    cy.visit('http://localhost:3000/659986/videos')
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/659986/videos', {
+      status: 200
+    })
+  });
+
+describe('Rancid Tomatillos movie trailer page', () => {
+  it('should be able to visit the movie trailer page', () => {
+    cy.get('header')
+      .contains('Rancid Tomatillos')
+  });
+
+  it('should be able to navigate to the movies page', () => {
+    cy.get('.view-btn')
+      .contains('View All Movies')
+      .click()
+      .get('.random-movie-title')
+  });
+
+  it('should be able to navigate to the movie details page', () => {
+    cy.get('.view-btn')
+      .contains('View Movie Details')
+      .click()
+      .get('h1')
+      .contains('The Owners')
+  });
+});
